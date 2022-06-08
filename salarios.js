c8c1465a965aa1b33b39887395cdc1588d9ccbe7
Rod01Andres <andresmrodriguezr@outlook.com>
2022-06-08 10:25:22 -0500

    Ultimos cambios de los talleres.

---
+++ salarios.js
@@ -0,0 +1,43 @@
+const colombia = [];
+
+colombia.push({
+    name: "Camila",
+    salary: 500
+});
+colombia.push({
+    name: "Natalia",
+    salary: 5000
+});
+colombia.push({
+    name: "Sofia",
+    salary: 7000
+});
+colombia.push({
+    name: "Laura",
+    salary: 10000
+});
+colombia.push({
+    name: "Keisy",
+    salary: 4500
+});
+colombia.push({
+    name: "Juanse",
+    salary: 7000
+});
+colombia.push({
+    name: "Nico",
+    salary: 2000
+});
+colombia.push({
+    name: "Felipe",
+    salary: 3400
+});
+colombia.push({
+    name: "Danilo",
+    salary: 8000
+});
+colombia.push({
+    name: "Andres",
+    salary: 9400
+});
+
